9c90bd3f4e1095f5d96cf04b330ee74df8026bf5
Mo-Se7s <ms.alwear47@gmail.com>
2025-09-21 17:11:20 +0300

    adding the service section and make it responsive

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // Toggle navigation menu
 
 const toggleMenu = document.querySelector(".toggle-menu");
@@ -46,3 +48,27 @@ links.forEach((link) => {
     this.classList.add("active");
   };
 });
+
+function alphabetPosition(text) {
+  let alphbet = "abcdefghijklmnopqrstuvwxyz";
+  return text
+    .toLowerCase()
+    .split("")
+    .map((char) => alphbet.indexOf(char) + 1)
+    .filter((num) => num > 0)
+    .join(" ");
+}
+
+// console.log(alphabetPosition("The sunset sets at twelve o' clock."));
+let a = "acdeghilnorstu"
+
+function longest(s1, s2) {
+  let sorted = [];
+  (s1+s2).split("").filter((char) => {
+      if (!sorted.includes(char)) {
+        sorted.push(char);
+      }
+    });
+  return sorted.sort().join("");
+}
+console.log(longest(a, a));
